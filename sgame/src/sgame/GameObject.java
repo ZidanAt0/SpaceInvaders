94c8d01bb1358a1a34f078cbacd5893660ff3707
Zidan Ahmad At-thoriq <zidanat6000@gmail.com>
2024-12-13 15:45:55 +0700

    Update GameObject.java

--- sgame/src/sgame/GameObject.java
+++ sgame/src/sgame/GameObject.java
@@ -12,4 +12,15 @@ public abstract class GameObject {
         this.width = width;
         this.height = height;
     }
+
+    public double getX() { return x; }
+    public void setX(double x) { this.x = x; }
+    public double getY() { return y; }
+    public void setY(double y) { this.y = y; }
+    public double getWidth() { return width; }
+    public void setWidth(double width) { this.width = width; }
+    public double getHeight() { return height; }
+    public void setHeight(double height) { this.height = height; }
+
+    public abstract void update();
 }
