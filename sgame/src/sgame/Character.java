f112d4ac0cdb82ad766b0c6ace1a2e5ed37767a2
Zidan Ahmad At-thoriq <zidanat6000@gmail.com>
2024-12-13 15:49:24 +0700

    Update Character.java

--- sgame/src/sgame/Character.java
+++ sgame/src/sgame/Character.java
@@ -19,4 +19,17 @@ public abstract class Character extends GameObject {
         this.isAlive = true;
         this.currentFrame = 0;
     }
+
+    public double getHitboxWidth() { return hitboxWidth; }
+    public double getHitboxHeight() { return hitboxHeight; }
+    public double getSpeed() { return speed; }
+    public void setSpeed(double speed) { this.speed = speed; }
+    public boolean isAlive() { return isAlive; }
+    public void setAlive(boolean alive) { isAlive = alive; }
+    public Image[] getSprites() { return sprites; }
+    public void setSprites(Image[] sprites) { this.sprites = sprites; }
+    public int getCurrentFrame() { return currentFrame; }
+    public void setCurrentFrame(int frame) { this.currentFrame = frame; }
+
+    public abstract void handleCollision();
 }
