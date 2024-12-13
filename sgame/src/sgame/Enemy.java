a4c824bda018b820a41b468d4827c2d320ecf433
citrafardiani-24 <148729858+citrafardiani-24@users.noreply.github.com>
2024-12-13 16:23:28 +0700

    Update Enemy.java

--- sgame/src/sgame/Enemy.java
+++ sgame/src/sgame/Enemy.java
@@ -1,1 +1,27 @@
+package sgame;
 
+import javafx.scene.image.Image;
+
+public class Enemy extends Character {
+    private Image image;
+    private int shootInterval;
+    
+    public Enemy(double x, double y, Image image) {
+        super(x, y, 40, 40, 30, 30, 0.5);
+        this.image = image;
+        this.shootInterval = 240;
+    }
+
+    public Image getImage() { return image; }
+    public int getShootInterval() { return shootInterval; }
+
+    @Override
+    public void update() {
+        setY(getY() + getSpeed());
+    }
+
+    @Override
+    public void handleCollision() {
+        setAlive(false);
+    }
+}
