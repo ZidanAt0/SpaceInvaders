c40b3c6fa60fe0acaf85c450e8432dc1a75a5b45
Melissa_21 <151343685+melissa211104@users.noreply.github.com>
2024-12-13 15:01:00 +0700

    Update MenuController.java

--- sgame/src/sgame/MenuController.java
+++ sgame/src/sgame/MenuController.java
@@ -1,1 +1,26 @@
+package sgame;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class MenuController {
+    @FXML private ImageView playgame;
+    @FXML private ImageView exitgame;
+
+    @FXML
+    private void startgame(MouseEvent event) {
+        try {
+            Sgame.switchToGame();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void keluar(MouseEvent event) {
+        Stage stage = (Stage) exitgame.getScene().getWindow();
+        stage.close();
+    }
+}
